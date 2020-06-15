1d3f0437736072d4f1e7effd2f06d33299db7415
David Coeurjolly <david.coeurjolly@liris.cnrs.fr>
2020-06-15 15:46:02 +0200

    DGtal

---
+++ test-with-externalproject/simpleTest2.cpp
@@ -0,0 +1,11 @@
+#include <DGtal/base/Common.h>
+#include <DGtal/helpers/StdDefs.h>
+
+using namespace DGtal;
+
+int main()
+{
+  trace.info()<<" Point "<<Z3i::Point(1,2,3)<<std::endl;
+  return 0;
+
+}
